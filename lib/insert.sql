4a0cf0200ce806da8250ca5ee138502e7267863a
Nabil Hayet <nabil.bracu@gmail.com>
2020-05-29 21:30:56 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+insert into bears values("1","Mr. Chocolate","3","male","white","cool","yes");
+insert into bears values("2","Rowdy","4","male","white","angry","yes");
+insert into bears values("3","Tabitha","3","male","white","cool","yes");
+insert into bears values("4","Sergeant Brown","3","male","white","cool","yes");
+insert into bears values("5","Melissa","3","male","white","cool","yes");
+insert into bears values("6","Grinch","3","male","white","cool","yes");
+insert into bears values("7","Wendy","3","male","white","cool","yes");
+insert into bears values("8",NULL,"3","male","white","cool","yes");
